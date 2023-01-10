c3542d6e57ed49e318395e8555a232fd89672e5c
Cesar Vinicius <cesarvinici@gmail.com>
2023-01-10 20:04:05 -0300

    refactor no build do node

--- docker/desafios/node/index.js
+++ docker/desafios/node/index.js
@@ -10,6 +10,18 @@ const mysqlConfig = {
     database: 'nodedb'
 };
 
+function newConnection() {
+    return mysql.createConnection(mysqlConfig);
+}
+
+
+async function createTable() {
+    const sql = 'CREATE TABLE IF NOT EXISTS people (id integer auto_increment primary key, name varchar(255))';
+    const connection = newConnection();
+    connection.query(sql);
+    connection.end();
+}
+
 async function getNameFromApi() {
     const response = await axios.get('https://gerador-nomes.wolan.net/nomes/1');
     return response.data
@@ -18,7 +30,7 @@ async function getNameFromApi() {
 async function saveNewNameOnDatabase() {
     const name = await getNameFromApi();
 
-    const connection = mysql.createConnection(mysqlConfig)
+    const connection = newConnection()
     const sql = `INSERT into people(name) values ("${name}")`;
     connection.query(sql);
     connection.end();
@@ -29,9 +41,11 @@ port = 3000;
 
 app.get('/', async (req, res) => {
 
+    await createTable();
+
     await saveNewNameOnDatabase();
 
-    const connection = mysql.createConnection(mysqlConfig)
+    const connection = newConnection();
 
     const sql = `SELECT name from people`;
     connection.query(sql, async function (error, results, fields) {
