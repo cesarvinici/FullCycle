63c6610920e3fa238921fa7380f533c7b4a44097
Cesar Vinicius <cesarvinici@gmail.com>
2023-09-11 21:03:15 +0100

    fixing laravel env

--- codeflix/micro-admin-videos/.docker/mysql/initdb.sql
+++
@@ -1,1 +0,0 @@
-CREATE DATABASE IF NOT EXISTS code_micro_videos;
--- codeflix/microservice-catalog-video/src/Core/Domain/Exception/EntityValidationException.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Core\Domain\Exception;
-
-use Exception;
-
-class EntityValidationException extends Exception
-{}
